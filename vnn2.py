09c934ed8ea733427eb177ff9f52c42208510d52
David Clark <dgc2138@cumc.columbia.edu>
2021-04-06 23:14:24 -0400

    Get Local2d stuff working

--- vnn2.py
+++ vnn2.py
@@ -256,6 +256,96 @@ class Conv2d(nn.Module):
 Vectorized locally connected layer
 """
 
+def lc_forward(input, weight, bias=None, stride=1, padding=0):
+    #input = (batch_size, in_channels, h_in, w_in)
+    #weight = (out_channels, h_out, w_out, in_channels, kernel_size, kernel_size)
+    out_channels, h_out, w_out, in_channels, kernel_size = weight.shape[:-1]
+    batch_size = input.shape[0]
+    if padding > 0:
+        padder = nn.ZeroPad2d(padding)
+        padded_input = padder(input)
+    else:
+        padded_input = input
+    output = torch.zeros(batch_size, out_channels, h_out, w_out)
+    for i in range(h_out):
+        for j in range(w_out):
+            i1 = i*stride
+            i2 = i1 + kernel_size
+            j1 = j*stride
+            j2 = j1 + kernel_size
+            input_chunk = padded_input[:, :, i1:i2, j1:j2]
+            weight_for_chunk = weight[:, i, j]
+            output[:, :, i, j] = torch.einsum("oikl,bikl->bo", weight_for_chunk, input_chunk)
+    if bias is not None:
+        output = output + bias
+    return output
+    
+def lc_backward(grad_output, weight, input_shape, stride=1, padding=0):
+    #weight = (out_channels, h_out, w_out, in_channels, K, K)
+    in_channels, h_in, w_in = input_shape
+    batch_size, out_channels = grad_output.shape[:2]
+    grad_input = torch.zeros((batch_size, in_channels, h_in+2*padding, w_in+2*padding))
+    for k in range(kernel_size):
+        for l in range(kernel_size):
+            relevant_weight = weight[:, :, :, :, k, l]
+            result = torch.einsum("omnc,bomn->bcmn", relevant_weight, grad_output)
+            s1, s2 = result.shape[-2:]
+            print(result.shape, grad_input[:, :, stride*k:stride*k+s1, stride*l:stride*l+s2].shape)
+            grad_input[:, :, stride*k:stride*k+s1, stride*l:stride*l+s2] += result
+    if padding > 0:
+        grad_input = grad_input[:, :, padding:-padding, padding:-padding]
+    return grad_input
+
+def lc_compute_grads(input, grad_output, kernel_size, bias=False, stride=1, padding=0):
+        if padding > 0:
+            padder = nn.ZeroPad2d(padding)
+            padded_input = padder(input)
+        else:
+            padded_input = input
+        in_channels, h_in, w_in = input.shape[1:]
+        out_channels, h_out, w_out = grad_output.shape[1:]
+        delta_W = torch.zeros(out_channels, h_out, w_out, in_channels, kernel_size, kernel_size)
+        if bias:
+            delta_b = torch.zeros(out_channels, h_out, w_out)
+        else:
+            delta_b = None
+        for i in range(h_out):
+            for j in range(w_out):
+                i1 = i*stride
+                i2 = i1 + kernel_size
+                j1 = j*stride
+                j2 = j1 + kernel_size
+                input_chunk = padded_input[:, :, i1:i2, j1:j2] #batch, in_channels, K, K
+                post_chunk = grad_output[:, :, i, j] #batch, out_channels
+                delta_W_local = torch.einsum("bikl,bo->oikl", input_chunk, post_chunk) #/ padded_input.shape[0] #divide by batch size
+                delta_W[:, i, j] = delta_W_local
+                if bias:
+                    delta_b_local = post_chunk.sum(dim=0) #/ padded_input.shape[0] #divide by batch size
+                    delta_b[:, i, j] = delta_b_local
+        return delta_W, delta_b #return both grads
+
+class Local2dFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, weight, bias=None, stride=1, padding=0):
+        ctx.save_for_backward(input, weight)
+        ctx.has_bias = bias is not None
+        ctx.stride = stride
+        ctx.padding = padding
+        output = lc_forward(input, weight, bias=bias, stride=stride, padding=padding)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight = ctx.saved_tensors
+        has_bias, stride, padding = ctx.has_bias, ctx.stride, ctx.padding
+        grad_input = grad_weight = grad_bias = None
+        if ctx.needs_input_grad[0]:
+            grad_input = lc_backward(grad_output, weight, input.shape, stride=stride, padding=padding)
+        if ctx.needs_input_grad[1]:
+            grad_weight, grad_bias = lc_compute_grads(input, grad_output, weight.shape[-1],
+                bias=has_bias, stride=stride, padding=padding)
+        return grad_input, grad_weight, grad_bias, None, None
+
 class Local2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, h_in, w_in, stride=1, padding=0, bias=True):
         super().__init__()
@@ -272,33 +362,16 @@ class Local2d(nn.Module):
         self.h_out = h_out
         self.w_out = w_out
         k = in_channels*kernel_size**2
-        self.weight = nn.Parameter(torch.randn(h_out, w_out, out_channels, in_channels, kernel_size, kernel_size)/np.sqrt(k))
+        self.weight = nn.Parameter(torch.randn(out_channels, h_out, w_out, in_channels, kernel_size, kernel_size)/np.sqrt(k))
         if bias:
             self.bias = nn.Parameter(torch.zeros(out_channels, h_out, w_out))
+        else:
+            self.register_parameter('bias', None)
         
     def forward(self, input):
-        #input = (batch, in_channels, h_in, w_in)
-        batch_size = input.shape[0]
-        if self.padding > 0:
-            padder = nn.ZeroPad2d(self.padding)
-            padded_input = padder(input)
-        else:
-            padded_input = input
-        output = torch.zeros(batch_size, self.out_channels, self.h_out, self.w_out, device=input.device)
-        for i in range(self.h_out):
-            for j in range(self.w_out):
-                i1 = i*self.stride
-                i2 = i1 + self.kernel_size
-                j1 = j*self.stride
-                j2 = j1 + self.kernel_size
-                input_chunk = padded_input[:, :, i1:i2, j1:j2]
-                weight_for_chunk = self.weight[i, j] #, :, :, :]
-                output[:, :, i, j] = torch.einsum("oikl,bikl->bo", weight_for_chunk, input_chunk)
-        if self.has_bias:
-            output = output + self.bias[None, :, :, :]
-        return output
-    
-    
+        return Local2dFunction.apply(input, self.weight, self.bias, self.stride, self.padding)
+
+"""
 class VecLocal2d(nn.Module):
     def __init__(self, category_dim, in_channels, out_channels, kernel_size, h_in, w_in,
                  stride=1, padding=0,
@@ -378,7 +451,7 @@ class VecLocal2d(nn.Module):
                 delta_b[:, :, i, j] = delta_b_local
         set_or_add_grad(self.lc.weight, delta_W)
         set_or_add_grad(self.bias, delta_b)
-
+"""
 
 """
 Vectorized nonlinearities
